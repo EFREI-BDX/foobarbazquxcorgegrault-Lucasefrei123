28a7207b7d1e84bcca6bdb0b4d3996a980ba0d85
Lucasefrei123 <lucas.barthelemy2005@gmail.com>
2025-02-14 18:43:54 +0100

    test1

---
+++ src/test/java/com/jad/Corge.java
@@ -0,0 +1,19 @@
+package com.jad;
+
+public class Corge {
+
+    private Foo foo;
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
+
+
+
+    public Corge(Foo foo) {}
+
+}
---
+++ src/test/java/com/jad/Grault.java
@@ -0,0 +1,12 @@
+package com.jad;
+
+public class Grault {
+    private Foo foo;
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public Grault(Foo foo) {}
+
+}
